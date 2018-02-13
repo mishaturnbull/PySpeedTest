29018ade211945352a09759fac4b10f9a32597bf
Misha Turnbull <mishaturnbull@gmail.com>
2018-02-13 14:21:42 -0600

    Remove unnecessary bits in uploadclient.py

--- uploadclient.py
+++ uploadclient.py
@@ -6,7 +6,7 @@ Created on Thu Jan 25 20:07:11 2018
 """
 
 import socket
-import copy
+
 try:
     import tkinter.messagebox as messagebox
 except ImportError:
@@ -17,14 +17,10 @@ from settings import REC_FILE, UPLOAD_URL, UPLOAD_PORT
 def upload():
     with open(REC_FILE, 'r') as f:
         lines = f.readlines()
-    outlines = copy.copy(lines)
     try:
-        print("Pre-setup")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(30)  # it's slow...
-        print("Socket created: " + repr(socket))
         s.connect((UPLOAD_URL, int(UPLOAD_PORT)))
-        print("Connected")
         s.sendall(bytes(''.join(lines), 'ascii'))
 
     except Exception as exc:  # too general, I know... working on it.
