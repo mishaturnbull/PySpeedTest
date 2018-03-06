1003d59345d082bd869f1e4f0193c6325f20f117
Misha Turnbull <mishaturnbull@gmail.com>
2018-03-06 01:17:22 -0600

    Add graphical error display helper
    I can never seem to get it right...

---
+++ src/errors.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Helpers for graphical error messages.
+
+Should not require any 3rd-party or unsafe modules.
+
+Py6 compliant.
+"""
+
+try:
+    import tkinter as tk
+    import tkinter.messagebox as messagebox
+except ImportError:
+    import Tkinter as tk
+    import tkMessageBox as messagebox
+
+# py2 proofing
+import sys
+
+def display_error(exception):
+    
+    message = "wut"
+    if sys.version_info[0] == 2:
+        message = exception.message
+    else:
+        # assume python 3, doubt many are running py1...
+        message = exception.args[0]
+    
+    messagebox.showerror('Error', message)
+    
+    raise exception
