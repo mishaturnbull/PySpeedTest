28aeab696c108c49ea956c085b39c991754c37a7
Misha Turnbull <mishaturnbull@gmail.com>
2018-03-23 13:26:43 -0500

    Add resource_path function to persistence.py
    See discussion in issue no. 37.

--- src/persistence.py
+++ src/persistence.py
@@ -55,3 +55,13 @@ if _IS_MAC:
         # live
         # nothing special needed
         pass
+
+# stolen this bit of code from a reply to PyInstaller issue #1804
+# https://github.com/pyinstaller/pyinstaller/
+# issues/1804#issuecomment-332778156
+# thanks, @StefGre!
+def resource_path(relative_path): # needed for bundling                                                                                                                            
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(
+            os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
