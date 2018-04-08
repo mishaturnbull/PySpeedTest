d700c0291ffefd38a1915c1c1880f0aef1da0ad1
Misha Turnbull <mishaturnbull@gmail.com>
2018-04-08 12:06:57 -0500

    Reduce version number for testing, in order to test properly

--- src/__version__.py
+++ src/__version__.py
@@ -7,7 +7,7 @@ Created on Sun Feb  4 15:11:57 2018
 
 __major__ = 2
 __minor__ = 0
-__patch__ = 1
+__patch__ = 0
 
 __version__ = '.'.join(map(str, [__major__, __minor__, __patch__]))
 __int_version__ = int(''.join(map(str, [__major__, __minor__, __patch__])))
@@ -19,7 +19,7 @@ __url__ = "https://github.com/mishaturnbull/PySpeedTest"
 
 def is_version_greater(ver1, ver2, safe=True):
     """
-    Assumes ver1 and ver2 are both of the form a.b.c
+    Assumes ver1 and ver2 are both of the form "v"a.b.c
     """
     maj1, min1, pat1 = [int(i) for i in ver1[1:].split('-')[0].split('.')]
     maj2, min2, pat2 = [int(i) for i in ver2[1:].split('-')[0].split('.')]
